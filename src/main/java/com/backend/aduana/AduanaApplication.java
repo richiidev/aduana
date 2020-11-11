5f28126ee12476d9fd24f6349bb400257e956ae5
lvindoor <jesus.garcia1953@alumnos.udg.mx>
2020-11-11 16:52:54 -0600

    configuracion de demonio

--- src/main/java/com/backend/aduana/AduanaApplication.java
+++ src/main/java/com/backend/aduana/AduanaApplication.java
@@ -1,20 +1,20 @@
 package com.backend.aduana;
-
-
-
-
-
-
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.concurrent.ExecutorService;
 
 @SpringBootApplication
 public class AduanaApplication {
 
+	private ExecutorService executor;
+	
 	public static void main(String[] args)  {
-		SpringApplication.run(AduanaApplication.class, args);	
-		
+		SpringApplication.run(AduanaApplication.class, args);			
 	}	
+
+	public boolean shutDown() {
+		executor.shutdown();
+		return true;
+	}
+
 }
